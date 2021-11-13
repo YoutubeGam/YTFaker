79f017b0b3cb44d882029aa17c915bbfb6721641
YoutubeGam <62714233+YoutubeGam@users.noreply.github.com>
2021-11-13 10:13:49 -0300

    Update script.lua

--- tutorais/Como postar seus script em Repositórios/script.lua
+++ tutorais/Como postar seus script em Repositórios/script.lua
@@ -1,3 +1,3 @@
-local link='https://raw.githubusercontent.com/YoutubeGam/YTFaker/main/Hello%20World.lua'
+local link='https://raw.githubusercontent.com/YoutubeGam/YTFaker/main/tutorais/Como%20postar%20seus%20script%20em%20Reposit%C3%B3rios/Hello%20world.lua'
 
 loadstring(game:HttpGet(link))()
